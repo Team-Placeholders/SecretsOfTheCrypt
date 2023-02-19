d42c1b73ea8d5428834adc5721027d834dc68307
DavidCTech <davidholmantech@gmail.com>
2023-02-19 00:19:30 -0500

    init commit

---
+++ Secrets of the Crypt/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    //public NavMeshAgent enemy;
+    //public Transform Player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //enemy.SetDestination(Player.position);
+    }
+}
