1f080fde253ab77fe27918104ed5d20f63894b5d
DavidCTech <davidholmantech@gmail.com>
2023-02-22 21:20:45 -0500

    init commit

--- Secrets of the Crypt/Assets/Scripts/Enemy.cs
+++ Secrets of the Crypt/Assets/Scripts/Enemy.cs
@@ -5,17 +5,94 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public NavMeshAgent enemy;
-    public Transform Player;
+    public NavMeshAgent agent;
+    public Transform player;
 
-    void Start()
+    //Patroling
+    public Transform[] points;
+    private int destPoint = 0;
+
+    //LayerMask
+    public LayerMask whatisPlayer;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
     {
-        
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemy.SetDestination(Player.position);
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatisPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatisPlayer);
+
+        if (!playerInAttackRange && !playerInSightRange)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+
+
+    }
+    private void AttackPlayer()
+    {
+
+        //Make sure enemy doesn't move
+        //agent.SetDestination(transform.position);
+        agent.SetDestination(player.position);
+
+
+
+
+        if (!alreadyAttacked)
+        {
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
